2d32c403dac4af4e1abbdaf19b7780ebeefd7f9a
Bartek <bartek.paczek@gmail.com>
2020-12-16 00:50:04 +0100

    done done

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,8 +9,7 @@
 std::vector< char > foo(std::list< Human >& people)
 {
     std::vector< char > ret_v(people.size());
-
-    // Twoja implementacja tutaj
-
+    std::for_each(people.begin(),people.end(),[](Human& person){person.birthday();});
+    std::transform(people.rbegin(),people.rend(),ret_v.begin(),[](Human& person){return person.isMonster()? 'n':'y';});
     return ret_v;
 }
